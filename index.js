c7d2654d4ce8ab5a70c43740a8704770eae5e873
taito9000 <ada.0205.tai120@gmail.com>
2022-10-06 22:55:34 +0900

    VanillaTodo_first_commit;

---
+++ index.js
@@ -0,0 +1,79 @@
+import "./styles.css";
+
+//  var val1 = "var 変数";
+// console.log(val1);
+
+// //var変数を上書き
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// //var変数は再宣言可能
+// var var1 = "var変数を再宣言";
+// console.log(var1);
+
+//  let val2 = "let変数";
+//  console.log(val2);
+
+//  //let変数は上書き可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// //let変数は再宣言不可
+// let val2 = "let変数を再宣言";
+// console.log(val2);
+
+//const val3 = "const変数";
+//console.log(val3);
+
+//const変数は上書き不可
+//val3 = "const変数を上書き";
+
+//const val3 = "const変数を再宣言";
+
+// const val4 = {
+//   name: "じゃけ",
+//   age: 28,
+// };
+
+// val4.name = "jak";
+// val4.address = "Kanagawa";
+// console.log(val4);
+
+// const val5 = ["dog","cat"];
+// val5[0]="bird";
+// val5.push("monkey");
+// console.log(val5);
+
+//テンプレート文字列
+// const name = "john";
+// const age = 28;
+
+// const message = `私の名前は${name}です。年齢は${age}です。`;
+// console.log = message;
+
+//従来の関数
+
+// function func1(str){
+//   return str;
+// };
+// console.log(func1("sss"));
+
+// //アロー関数
+
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2desu"));
+
+// //const func3 = (num1,num2) => {
+//  //return num1 + num2;
+// //};
+// //console.log(func3(10,11));
+
+// const func3 =(num1,num2) => num1 + num2;
+// console.log(func3(11,14));
+
+const myProfile = {
+  name: "jack",
+  age: 22
+};
